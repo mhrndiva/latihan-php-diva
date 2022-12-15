3e7aa67571ee1ac6f8ef866920323e6dcb8fb32a
mhrndiva <ariadiva00@gmail.com>
2022-12-15 16:06:17 +0700

    latihan php

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ini Latihan PHP</title>
+</head>
+<body>
+   <h1>Hello World</h1>
+   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem laudantium adipisci sapiente odit provident fugit, officiis ea iure. Quaerat voluptas delectus nisi illo facere ullam explicabo voluptatum enim nemo aspernatur!</p> <br>
+   <!-- ini adalah adalah codingan PHP -->
+   <?php
+   print "<h2>PHP is fur</h2>";
+   print "Hello World!";
+   print "I'm about to learn PHP";
+
+   ?>
+
+</body>
+</html>
