465f5675e79099c57e2b8494592ce8aee377873c
mhrndiva <ariadiva00@gmail.com>
2022-12-17 15:50:09 +0700

    tugas PHP

---
+++ latihan3.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Latihan 3</title>
+</head>
+<body>
+    <h2>Latihan 3 </h2>
+    <?php
+    $pembelian=75000;
+
+    if($pembelian>=100000)
+    {
+        $diskon=$pembelian*(25/100);
+    }elseif($pembelian>=75000)
+    {
+        $diskon=$pembelian*(20/100);
+    }elseif($pembelian>=50000)
+    {
+        $diskon=$pembelian*(15/100);
+    }elseif($pembelian>=25000)
+    {
+        $diskon=$pembelian*(10/100);
+    }elseif($pembelian>=10000)
+    {
+        $dsikon=$pembelian*(5/100);
+    }
+    $pembayaran=$pembelian-$diskon;
+    echo("<pre>");
+    echo("Total Pembelian  = Rp.".$pembelian."<br>");
+    echo("Diskon           = Rp.".$diskon."<br>");
+    echo("Total Pembayaran = Rp.".$pembayaran);
+    echo("");
+    ?>
+</body>
+</html>
